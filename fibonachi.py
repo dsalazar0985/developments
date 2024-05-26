52b90c8225fa1bdc1cbe935e6fb3f92210483172
dsalazar0985 <dajosava@hotmail.com>
2024-05-25 20:15:19 -0600

    kk

---
+++ fibonachi.py
@@ -0,0 +1,18 @@
+def fibonacci_iterative(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+
+    return fib_sequence
+
+# Example usage:
+n = 50
+print(fibonacci_iterative(n))  # Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
